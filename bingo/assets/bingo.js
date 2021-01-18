24a9a7b8f4556ebaca3eb8bf9667dbd3aae60806
Caelan Borowiec <caelan@caelanb.me>
2021-01-18 15:07:48 -0500

    Wrap bingo generator in named function, run on button click and first load

--- bingo/assets/bingo.js
+++ bingo/assets/bingo.js
@@ -1,77 +1,86 @@
 $(document).ready(function() {
-  const Squares = (5 * 5);
-  const FreeSpace = true;
-  const Questions = [
-    "Ut",
-    "elementum",
-    "sit",
-    "amet",
-    "ante",
-    "sit",
-    "amet",
-    "facilisis.",
-    "Nulla",
-    "sit",
-    "amet",
-    "elit",
-    "non",
-    "erat",
-    "erata",
-    "commodo",
-    "aliquet",
-    "et",
-    "eu",
-    "diam.",
-    "Ut",
-    "sollicitudin",
-    "urna",
-    "ex.",
-    "Duis."
-  ];
 
-  const cellsPerRow = Math.sqrt(Squares);
-  const star = Math.ceil((cellsPerRow / 2));
-  const shuffledQuestions = shuffleArray(Questions);
+  const DrawBingoSheet = function(Questions = false) {
+    const Squares = (5 * 5);
+    const FreeSpace = true;
+    if (!Questions)
+      Questions = [
+        "Ut",
+        "elementum",
+        "sit",
+        "amet",
+        "ante",
+        "sit",
+        "amet",
+        "facilisis.",
+        "Nulla",
+        "sit",
+        "amet",
+        "elit",
+        "non",
+        "erat",
+        "erata",
+        "commodo",
+        "aliquet",
+        "et",
+        "eu",
+        "diam.",
+        "Ut",
+        "sollicitudin",
+        "urna",
+        "ex.",
+        "Duis."
+      ];
 
-  let x = 1;
-  let y = 1;
-  let q = 0;
-  let questionBlocks = [];
+    const cellsPerRow = Math.sqrt(Squares);
+    const star = Math.ceil((cellsPerRow / 2));
+    const shuffledQuestions = shuffleArray(Questions);
 
-  for (var i = 0; i < Squares; i++) {
-    if (x > 5) {
-      x = 1;
-      y++;
+    let x = 1;
+    let y = 1;
+    let q = 0;
+    let questionBlocks = [];
+
+    for (var i = 0; i < Squares; i++) {
+      if (x > 5) {
+        x = 1;
+        y++;
+      }
+
+      questionBlocks[i] = $('<div>').addClass('square');
+      $span = $('<span>').addClass('question');
+
+      if (FreeSpace && x == star && y == star)
+        $span.html("&#9733;").addClass('star');
+      else {
+        $span.text(shuffledQuestions[q]);
+        q++
+      }
+
+      questionBlocks[i].html($span);
+
+      x++;
     }
 
-    questionBlocks[i] = $('<div>').addClass('square');
-    $span = $('<span>').addClass('question');
-
-    if (FreeSpace && x == star && y == star)
-      $span.html("&#9733;").addClass('star');
-    else {
-      $span.text(shuffledQuestions[q]);
-      q++
-    }
-
-    questionBlocks[i].html($span);
-
-    x++;
+    $('.generator').html(questionBlocks);
+    $('.square').on('click', function() {
+      $(this).toggleClass('ticked');
+    });
   }
 
-  $('.generator').html(questionBlocks);
-  $('.square').on('click', function() {
-    $(this).toggleClass('ticked');
-  })
   $('.collapse').on('click', function() {
-
     $('.inputBox').toggleClass('collapsed');
     if ($('.inputBox').hasClass('collapsed'))
       $(this).html('&laquo;')
     else
       $(this).html('&raquo;')
-  })
+  });
 
+  $('#generate').on('click', function() {
+    DrawBingoSheet(); // TODO: add input from textarea
+  });
+
+  DrawBingoSheet();
 });
 
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
